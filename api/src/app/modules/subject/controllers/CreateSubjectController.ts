acdce4c2ff39436371a4a4b5b5952120ab042825
Nathan Pereira <nathan.pereira@escoladnc.com.br>
2024-06-14 23:53:21 -0300

    refact: change form of json in createSubjectController

--- api/src/app/modules/subject/controllers/CreateSubjectController.ts
+++ api/src/app/modules/subject/controllers/CreateSubjectController.ts
@@ -1,17 +1,19 @@
 import { HttpResponse } from '../../../../app/presentation/protocols/http'
 import { Controller } from '../../../presentation/protocols/controller'
-import { CreateSubjectDTO } from '../../../../app/domain/dtos/createSubjectDTO'
+import { CreateSubjectDTO } from '../../../../app/domain/dtos/CreateSubjectDTO'
 import { badRequest, ok } from '../../../helpers/http-status'
 
 export class CreateSubjectController implements Controller {
   constructor() {}
 
   async handle(httpRequest: CreateSubjectDTO): Promise<HttpResponse> {
-    if(!httpRequest.body.name) {
+    const { name, quantityOfHours } = httpRequest.body
+    
+    if(!name) {
       return badRequest(new Error('Name should be provided'))
     }
 
-    if(httpRequest.body.quantityOfHours != 72 && httpRequest.body.quantityOfHours != 36) {
+    if(quantityOfHours != 72 && quantityOfHours != 36) {
       return badRequest(new Error('The quantity of hours must be 36 or 72'))
     }
 
