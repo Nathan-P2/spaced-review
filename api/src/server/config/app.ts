15fa35dc89290b69a155f67abdcfa829c8d66c0f
Nathan Pereira <nathan.pereira@escoladnc.com.br>
2024-06-15 23:59:36 -0300

    feat: add swagger doc generation

--- api/src/server/config/app.ts
+++ api/src/server/config/app.ts
@@ -1,5 +1,7 @@
 import express, { Express } from 'express'
 import router from './routes'
+import swaggerUi from "swagger-ui-express";
+import swaggerOutput from "../../../swagger_output.json";
 
 export const setupApp = async (port: number): Promise<Express> => {
   const app = express()
@@ -8,5 +10,6 @@ export const setupApp = async (port: number): Promise<Express> => {
   app.listen(port, () => {
     console.log(`Server started on port ${port}`)
   })
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput));
   return app
 }
